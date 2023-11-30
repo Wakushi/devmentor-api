092b4b3d27860137c32a7b722e8848261d0f23b8
Wakushi <maxime.eliazord@sequodia.com>
2023-11-30 01:29:16 +0100

    Updated main with cors

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,8 @@ dotenv.config();
 
 async function bootstrap() {
   console.log('Bootstrapping...');
-  let httpsOptions: HttpsOptions;
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    // cors: {
-    //   origin: ['http://localhost:4200', 'http://localhost:3000'],
-    //   methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    //   allowedHeaders: ['Content-Type', 'Authorization'],
-    //   exposedHeaders: ['Authorization'],
-    //   credentials: true,
-    // },
-    httpsOptions,
+    cors: true,
   });
 
   const port = process.env.PORT || 3000;
