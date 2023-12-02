74aa44a6da2323140109458f938116ef0a680f28
Wakushi <maxime.eliazord@sequodia.com>
2023-12-02 01:20:38 +0100

    Fixed LockedService import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MailerModule } from './modules/mailer/mailer.module';
 import { FirebaseModule } from './firebase/firebase.module';
+import { LockService } from './services/lock.service';
 
 @Module({
   imports: [MailerModule, ConfigModule.forRoot(), FirebaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LockService],
+  exports: [LockService],
 })
 export class AppModule {}
