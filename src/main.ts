e34e9f7a93e7624b53c91878898f0be6f52a3762
Wakushi <maxime.eliazord@sequodia.com>
2023-12-03 13:54:55 +0100

    Proxy issue fix

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
-  app.set('trust proxy', true);
+  app.set('trust proxy', 1);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
